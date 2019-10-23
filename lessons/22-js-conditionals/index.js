299f026c4d213df6ccb033db2a70d156304932ed
Azadeh Hatef <hatef.azadeh@gmail.com>
2019-10-22 20:03:21 -0600

    22-js-conditional

---
+++ lessons/22-js-conditionals/index.js
@@ -0,0 +1,12 @@
+alert("Enter two numbers to see if the first is greater than the second");
+let firstNumber = prompt('enter the first number');
+let secondNumber = prompt('enter the second number');
+if (firstNumber > secondNumber) {
+    alert("is greater");
+}
+else if (firstNumber < secondNumber) {
+    alert("it is less than");
+}
+else {
+    alert("its the same");
+}
